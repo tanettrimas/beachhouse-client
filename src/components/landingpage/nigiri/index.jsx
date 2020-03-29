5717e9aa8a1ce504b21d990f1d60792b9a2461e2
Tanet Trimas <tanet@trimas.no>
2020-03-29 04:26:50 +0200

    add proptypes to component

--- src/components/landingpage/nigiri/index.jsx
+++ src/components/landingpage/nigiri/index.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import './style.css'
-import { uppercaseString } from '../../../utils/uppercaseString'
+import PropTypes from 'prop-types';
 
 const NigiriComponent = ({ title, description, heading, imgSrc } = {}) => {
   return (
     <div className=''>
       <div className="regular-dealcard grid-component">
         <img src={imgSrc} alt="Nigiri" width="350px" height="250px" className="img"></img>
-        <h3 className="card-heading">{uppercaseString(heading)}</h3>
-        <h1 className="card-title">{uppercaseString(title)}</h1>
+        <h3 className="card-heading">{heading.toUpperCase()}</h3>
+        <h1 className="card-title">{title.toUpperCase()}</h1>
         <p className="card-description">{description}</p>
-        <button className="card-button">View more</button>
+        <button className="card-button">{"View more".toUpperCase()}</button>
       </div>
     </div>
     )
+}
+
+NigiriComponent.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  heading: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired
 }
 
 export default NigiriComponent
