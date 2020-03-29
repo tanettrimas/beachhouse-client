da70251dfc5c6fd54c021b6ae2204516171d4f0d
Tanet Trimas <tanet@trimas.no>
2020-03-29 16:35:28 +0200

    Bugfix: Fixed styling and re-poz of elements

--- src/components/landingpage/nigiri/index.jsx
+++ src/components/landingpage/nigiri/index.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 
 const NigiriComponent = ({ title, description, heading, imgSrc } = {}) => {
   return (
-    <section className="regular-dealcard grid-component">
-      <img src={imgSrc} alt="Nigiri" className="img"></img>
-      <div className="card-content">
-        <h3 className="card-heading">{heading.toUpperCase()}</h3>
-        <h1 className="card-title">{title.toUpperCase()}</h1>
-        <p className="card-description">{description}</p>
-        <button className="card-button">{"View more".toUpperCase()}</button>
-      </div>
+    <section className="regular-dealcard landing-page-wrapper grid-component"> 
+        <img src={imgSrc} alt="Nigiri" className="img"></img>
+        <div className="card-content">
+          <h3 className="card-heading">{heading.toUpperCase()}</h3>
+          <h1 className="card-title">{title.toUpperCase()}</h1>
+          <p className="card-description">{description}</p>
+          <button className="card-button">{"View more".toUpperCase()}</button>
+        </div>
     </section>
     )
 }
