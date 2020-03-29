0a022f33f679b66f48b60b87fda3771f0b70a279
Khoi Hoang <hello@koicha.dev>
2020-03-29 01:34:29 +0100

    Added Icon Component for the SideBar
    Bug: Need to fix Corrected vector color for each of the icon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import nigiriImg from './img/nigiri.jpg'
-import NigiriComponent from './components/landingpage/nigiri'
-
+import { SideBar } from './components/Sidebar/Sidebar.component';
 
 function App() {
   return (
-      <main className='container'>
-        <div className="sidebar grid-component">Some sidecard</div>
-        <div className="headercard grid-component">Some headercard regular</div>
-        <NigiriComponent
-          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-          heading="Appetizer"
-          imgSrc={nigiriImg}
-          title="Extravaganza"
-        />
-        <div className="sushi-dealcard grid-component">Some sushi data</div>
-        <div className="thai-dealcard grid-component">Some thai data</div>
-        <div className="sushimenu-card grid-component">Some sushimenu data</div>
-        <div className="thaimenu-card grid-component">Some thaimenu data</div>
-      </main>
+    <main className='container'>
+      <div className='sidebar grid-component'>
+        <SideBar />
+      </div>
+      <div className='headercard grid-component'>Some headercard regular</div>
+      <div className='regular-dealcard grid-component'>Some regular data</div>
+      <div className='sushi-dealcard grid-component'>Some sushi data</div>
+      <div className='thai-dealcard grid-component'>Some thai data</div>
+      <div className='sushimenu-card grid-component'>Some sushimenu data</div>
+      <div className='thaimenu-card grid-component'>Some thaimenu data</div>
+    </main>
   );
 }
 
