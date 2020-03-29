7d322eba1002129e8f6e5efc55ca5374057b7d78
Khoi Hoang <hello@koicha.dev>
2020-03-29 19:55:54 +0200

    Landing Page is created
    - Next step is to import font familiy manually

--- src/components/MainPage/Main-page.component.jsx
+++ src/components/MainPage/Main-page.component.jsx
@@ -5,10 +5,12 @@ import './main-page.style.css';
 export const LandingPage = () => {
   return (
     <section className='grid-container'>
-      <div className='left-landing-page' style={{ padding: '22.5rem 0' }}>
+      <div className='left-landing-page' style={{ padding: '17rem 0' }}>
         <h1>BEACH HOUSE</h1>
         <h2>Sushi og Thai Cuisine</h2>
-        <p>We deliver tasteful cross-over food</p>
+        <p style={{ paddingTop: '0.5rem' }}>We deliver tasteful</p>
+        <p style={{ margin: '0' }}>cross-over food</p>
+        <button className='card-button'>ORDER NOW</button>
       </div>
       <div className='right-landing-page' style={{ padding: '10.5rem 0' }}>
         <img src={ChefImg} alt='Beach House Sushi & Thai' />
